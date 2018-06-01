02887e12c69a45d91add3bff9941ddb70191e78d
Jack Lenox <jack@automattic.com>
2018-06-01 12:18:56 +0100

    Update header h1 conditions

--- header.php
+++ header.php
@@ -36,7 +36,7 @@
 		</div>
 
 		<?php
-		if ( is_front_page() && is_home() ) :
+		if ( is_front_page() && is_home() && ! get_query_var( 'menu' ) ) :
 			?>
 			<h1><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 			<?php
