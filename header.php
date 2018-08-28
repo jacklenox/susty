084977b5a2217acb023c8c2a675df33bf8992745
Jack Lenox <jack@automattic.com>
2018-08-28 14:57:04 +0100

    Update header to not show menu link if no menu exists

--- header.php
+++ header.php
@@ -46,20 +46,24 @@
 			<?php
 		endif;
 
-		if ( get_query_var( 'menu' ) ) :
-			?>
-			<a id="susty-back-link" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">&#x2716;<span class="screen-reader-text"><?php esc_html_e( 'Close menu', 'susty' ); ?></span></a>
-			<script>
-				var susty_home_url = '<?php echo esc_url( home_url( '/' ) ); ?>';
-				if ( 0 === document.referrer.indexOf( susty_home_url ) ) {
-					document.getElementById( 'susty-back-link' ).href = document.referrer;
-				}
-			</script>
-			<?php
-		else :
-			?>
-			<a href="<?php echo esc_url( ( get_option( 'permalink_structure' ) ? home_url( '/menu/' ) : home_url( '/?menu' ) ) ); ?>"><?php esc_html_e( 'Menu', 'susty' ); ?></a>
-			<?php
+		if ( has_nav_menu( 'menu-1' ) ) :
+
+			if ( get_query_var( 'menu' ) ) :
+				?>
+				<a id="susty-back-link" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">&#x2716;<span class="screen-reader-text"><?php esc_html_e( 'Close menu', 'susty' ); ?></span></a>
+				<script>
+					var susty_home_url = '<?php echo esc_url( home_url( '/' ) ); ?>';
+					if ( 0 === document.referrer.indexOf( susty_home_url ) ) {
+						document.getElementById( 'susty-back-link' ).href = document.referrer;
+					}
+				</script>
+				<?php
+			else :
+				?>
+				<a href="<?php echo esc_url( ( get_option( 'permalink_structure' ) ? home_url( '/menu/' ) : home_url( '/?menu' ) ) ); ?>"><?php esc_html_e( 'Menu', 'susty' ); ?></a>
+				<?php
+			endif;
+		
 		endif;
 		?>
 	</header>
